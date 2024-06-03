e9927fb2f35500b61ee67356a3e42983368bfd0c
tomas baute <tomasbaute14@gmail.com>
2024-06-03 15:37:45 -0300

    fix urls

--- WeatherAPI/urls.py
+++ WeatherAPI/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import health_check, custom_404, custom_500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
